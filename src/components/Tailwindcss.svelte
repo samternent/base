a9313e4d59aac38619886e110188535ba053eef5
Sam Ternent <sam.ternent@gmail.com>
2020-06-21 20:28:12 +0100

    remove body margin

--- src/components/Tailwindcss.svelte
+++ src/components/Tailwindcss.svelte
@@ -4,6 +4,9 @@
   @tailwind utilities;
 
   @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;500;800;900&display=swap');
+  html, body {
+    margin: 0;
+  }
   html, input {
     font-size: 18px;
     color: #2a2b2a;
